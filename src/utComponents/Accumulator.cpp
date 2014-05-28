f8046e04c092882ef1bab14b8fe19a4b36886396
Christian Waechter <christian.waechter@in.tum.de>
2014-05-28 17:14:53 +0200

    - changed Accumulator's implementation to little more efficient code - briefly checked that it is working - added some more console output information to initialization - it also seems to be quite similar to ringbuffer -> maybe merge code to one component sometime?

--- src/utComponents/Accumulator.cpp
+++ src/utComponents/Accumulator.cpp
@@ -36,26 +36,55 @@
  * @author Florian Echtler <echtler@in.tum.de>
  */
 
+// Ubitrack
 #include <utDataflow/PushConsumer.h>
 #include <utDataflow/PushSupplier.h>
 #include <utDataflow/Component.h>
 #include <utDataflow/ComponentFactory.h>
 #include <utMeasurement/Measurement.h>
 
+// Boost
 #include <boost/bind.hpp>
 
-// currently unused
-// #include <log4cpp/Category.hh>
-// static log4cpp::Category& logger( log4cpp::Category::getInstance( "Ubitrack.Components.Accumulator" ) );
+// log4cpp
+#include <log4cpp/Category.hh>
+static log4cpp::Category& logger( log4cpp::Category::getInstance( "Ubitrack.Events.Components.Accumulator" ) );
 
 namespace Ubitrack { namespace Components {
 
 using namespace Dataflow;
 
+/// @todo the implemented behaviour is similar to a ring buffer, which is implemented in a different component "ringbuffer", delete one component?
 template< class EventType >
 class Accumulator
 	: public Component
 {
+protected:
+
+	/** short-cut to the underlying mathematical construct encapsulated in the \c EventType measurement */
+	typedef typename EventType::value_type MathType;
+	
+	/** short-cut to a container that stores the underlying elements of the incoming \c EventTypes measurements */
+	typedef typename std::vector< MathType > EventTypeList;
+	
+	/** short-cut to a measurement encapsulating the container of \c EventTypes */
+	typedef typename Measurement::Measurement< EventTypeList > MeasurementEventTypeList;
+	
+	/** Maximal size of container where the elements are stored */
+	std::size_t m_maxLength;
+	
+	/** an index pointing to the next free position in container that can be overwritten */
+	std::size_t m_index;
+	
+	/** container storing all the elements pushed to this component */
+	EventTypeList m_dataStorage;
+
+	/** Outgoing port of this component */
+	PushSupplier< MeasurementEventTypeList > m_outPort;
+	
+	/** Incoming ports of this component */
+	std::vector< boost::shared_ptr< Dataflow::PushConsumer< EventType > > > m_inPorts;
+
 public:
 	/**
 	 * Standard component constructor.
@@ -66,11 +95,25 @@ public:
 	Accumulator( const std::string& nm, boost::shared_ptr< Graph::UTQLSubgraph > pCfg )
 		: Ubitrack::Dataflow::Component( nm )
 		, m_maxLength( 100 )
-		, m_size( 0 )
-		, m_data()
+		, m_index( 0 )
+		, m_dataStorage()
 		, m_outPort( "Output", *this )
 	{
 		pCfg->m_DataflowAttributes.getAttributeData( "maxLength", m_maxLength );
+		
+		// check for maximal storage size of container
+		const std::size_t maxSize = m_dataStorage.max_size();
+		if( maxSize < m_maxLength )
+			LOG4CPP_WARN( logger, "Cannot allocate storage space for " << m_maxLength << " elements of \"" << typeid( MathType ).name() << "\", there is capacity for only " << maxSize << " of them." );
+			
+		// reserve the expected space in advance
+		// reduces reallocation to zero -> expectation:faster		
+		m_maxLength = std::min( maxSize, m_maxLength );
+		m_dataStorage.reserve( m_maxLength );
+		
+		// adjust size once more to available storage capacity
+		m_maxLength = std::min( m_dataStorage.capacity(), m_maxLength );
+		LOG4CPP_INFO( logger, "Allocated space for storing " << m_maxLength << " elements of \"" << typeid( MathType ).name() << "\".");
 
 		for ( Graph::UTQLSubgraph::EdgeMap::iterator it = pCfg->m_Edges.begin(); it != pCfg->m_Edges.end(); it++ )
 			if ( it->second->isInput() )
@@ -88,31 +131,29 @@ protected:
 	/** called when a new item arrives */
 	void receive( const EventType& event )
 	{
-		m_data.push_back( *event );
-		if (++m_size > m_maxLength) {
-			m_data.erase( m_data.begin() );
-			m_size--;
+		const std::size_t n = std::distance( m_dataStorage.begin(), m_dataStorage.end() );
+		
+		// storage is not full, yet, insert elements at the end
+		if( n < m_maxLength )
+			m_dataStorage.push_back( *event );
+		else // storage is full
+		{
+			// overwrite elements from the beginning
+			m_dataStorage[ m_index++ ] = (*event);
+			// reset the index if at end
+			m_index = m_index % m_maxLength;
 		}
-		m_outPort.send( Measurement::Measurement< typename std::vector< typename EventType::value_type > >( event.time(), m_data ) );
+
+		m_outPort.send( MeasurementEventTypeList( event.time(), m_dataStorage ) );
 	}
-
-	/** Properties of the accumulator*/
-	std::size_t m_maxLength;
-	
-	std::size_t m_size;
-	std::vector< typename EventType::value_type > m_data;
-
-	/** Ports of the component */
-	PushSupplier< Measurement::Measurement< typename std::vector< typename EventType::value_type > > > m_outPort;
-	std::vector< boost::shared_ptr< Dataflow::PushConsumer< EventType > > > m_inPorts;
-
 };
 
 UBITRACK_REGISTER_COMPONENT( ComponentFactory* const cf ) 
 {
+	cf->registerComponent< Accumulator< Measurement::Pose > > ( "PoseAccumulator" );
 	cf->registerComponent< Accumulator< Measurement::Position > > ( "PositionAccumulator" );
 	cf->registerComponent< Accumulator< Measurement::Position2D > > ( "Position2DAccumulator" );
-	cf->registerComponent< Accumulator< Measurement::Pose > > ( "PoseAccumulator" );
+	
 }
 
 } } // namespace Ubitrack::Components
